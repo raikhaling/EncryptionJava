c74b84a558acb1ab133c282785b0c7ebc4c2cde6
Nabin Rai <rainabinkhyal@gmail.com>
2023-02-09 23:13:07 +0545

    Initial Commit
    pojo

---
+++ src/EncryptionClass.java
@@ -0,0 +1,29 @@
+
+public class EncryptionClass {
+	EncryptionClass(){
+		
+	}
+	
+	private void askQuestion() {
+		
+	}
+	
+	private void newKey() {
+		
+	}
+	
+	private void getKey() {
+		
+	}
+	
+	private void encrypt() {
+		
+	}
+	
+	private void decrypt() {
+		
+	}
+	private void quit() {
+		
+	}
+}
