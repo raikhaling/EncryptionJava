cc5ec5e436cca6ebf4018db74d2e74ff2a1f215f
Nabin Rai <rainabinkhyal@gmail.com>
2023-02-09 23:35:06 +0545

    askQuestion Module

--- src/EncryptionClass.java
+++ src/EncryptionClass.java
@@ -1,11 +1,57 @@
+import java.util.*;
 
 public class EncryptionClass {
+	
+	private Scanner scanner;
+	private Random random;
+	private ArrayList<Character> list;
+	private ArrayList<Character> shuffledList;
+	private char character; 
+	private String line;
+	private char[] letters;
+	private char[] secretLetters;
+ 	
+	
 	EncryptionClass(){
+		scanner = new Scanner(System.in);
+		random = new Random();
+		list = new ArrayList<>();
+		shuffledList = new ArrayList<>();
+		character = ' ';
 		
+		newKey();
+		askQuestion();
 	}
 	
 	private void askQuestion() {
-		
+		while(true) {
+			System.out.println("*********************************************");
+			System.out.println("What do you want to do?");
+			System.out.println("(N)ew Key,(G)etKey,(E)ncrypt,(D)ecrypt,(Q)uit");
+			char response = Character.toUpperCase(scanner.nextLine().charAt(0));
+			
+			switch(response) {
+			case 'N':
+				newKey();
+				break;
+			case 'G':
+				getKey();
+				break;
+			case 'E':
+				encrypt();
+				break;
+			case 'D':
+				decrypt();
+				break;
+				
+			case 'Q':
+				quit();
+				break;
+			default:
+				System.out.println("Not a valid anser....");
+				
+			}
+		}
 	}
 	
 	private void newKey() {
@@ -24,6 +70,6 @@ public class EncryptionClass {
 		
 	}
 	private void quit() {
-		
+		 
 	}
 }
